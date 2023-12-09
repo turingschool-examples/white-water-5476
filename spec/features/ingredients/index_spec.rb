dfa2d0cf6a137c5c5681ec8ff5662f4b0f2e2a82
Nathan Lambertson <lambertones@gmail.com>
2023-12-09 10:03:32 -0600

    Add testing to ingredients index spec for aplhabetical order

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "ingredients index page", type: :feature do
-  it "shows a list of all ingredients with their attributes" do
+  it "shows a list of all ingredients with their attributes, sorted alphabetically" do
     cheese = Ingredient.create!(name: "Cheese", cost: 3)
     noods = Ingredient.create!(name: "Noodles", cost: 2)
     herbals = Ingredient.create!(name: "Herbs", cost: 1)
@@ -18,5 +18,7 @@ RSpec.describe "ingredients index page", type: :feature do
     expect(page).to have_content("Cheese: 3")
     expect(page).to have_content("Noodles: 2")
     expect(page).to have_content("Herbs: 1")
+    expect(page.body.index("Cheese: 3")).to be < page.body.index("Herbs: 1")
+    expect(page.body.index("Herbs: 1")).to be < page.body.index("Noodles: 2")
   end
 end
