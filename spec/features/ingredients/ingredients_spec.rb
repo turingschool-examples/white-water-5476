939fd267aea4f3666ecafaffc94cf98ae2d02f8e
Nathan Lambertson <lambertones@gmail.com>
2023-12-09 08:36:20 -0600

    Add test for US 1

---
+++ spec/features/ingredients/ingredients_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "ingredients index page", type: :feature do
+  it "shows a list of all ingredients with their attributes" do
+    cheese = Ingredient.create!(name: "Cheese", cost: 3)
+    noods = Ingredient.create!(name: "Noodles", cost: 2)
+    herbals = Ingredient.create!(name: "Herbs", cost: 1)
+
+    visit "/ingredients"
+
+    expect(current_path).to eq("/ingredients")
+    expect(page).to have_content(cheese.name)
+    expect(page).to have_content(noods.name)
+    expect(page).to have_content(herbals.name)
+    expect(page).to have_content(cheese.cost)
+    expect(page).to have_content(noods.cost)
+    expect(page).to have_content(herbals.cost)
+  end
+end
