1656068ae8cc46a9a3ebcf3af5e3fa5b86fbb49d
Dylan Perry <dylanbperry@gmail.com>
2023-12-10 14:38:11 -0800

    Add index method to controller and default sorting to model

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,5 @@
 class IngredientsController < ApplicationController
     def index
-        @ingredients = Ingredient.all
+        @ingredients = Ingredient.order_by_recently_created
     end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,7 @@
 class Ingredient < ApplicationRecord
    validates_presence_of :name, :cost
+
+   def self.order_by_recently_created
+      order(created_at: :DESC)
+   end
 end
