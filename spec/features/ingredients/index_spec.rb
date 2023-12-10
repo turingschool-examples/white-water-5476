f4a9a467a9503cc6e8a6ee30c9be14267e560de0
Dylan Perry <dylanbperry@gmail.com>
2023-12-10 14:51:04 -0800

    Add test for index sorting default (created by)

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -1,18 +1,29 @@
 require "rails_helper"
 
 describe "the ingredients index page" do
-    it "lists all the ingredients with their attributes" do
-      ground_beef = Ingredient.create(name: "Ground Beef", cost: 4)
-      garlic = Ingredient.create(name: "Garlic", cost: 2)
-      tomato_paste = Ingredient.create(name: "Tomato Paste", cost: 1)
-      
-      visit "/ingredients"
+  it "lists all the ingredients with their attributes" do
+    ground_beef = Ingredient.create(name: "Ground Beef", cost: 4)
+    garlic = Ingredient.create(name: "Garlic", cost: 2)
+    tomato_paste = Ingredient.create(name: "Tomato Paste", cost: 1)
+    
+    visit "/ingredients"
 
-      expect(page).to have_content("Ground Beef")
-      expect(page).to have_content("Garlic")
-      expect(page).to have_content("Tomato Paste")
-      expect(page).to have_content("4")
-      expect(page).to have_content("2")
-      expect(page).to have_content("1")
-    end
+    expect(page).to have_content("Ground Beef")
+    expect(page).to have_content("Garlic")
+    expect(page).to have_content("Tomato Paste")
+    expect(page).to have_content("4")
+    expect(page).to have_content("2")
+    expect(page).to have_content("1")
+  end
+
+  it "sorts the index by last created ingredient" do
+    ground_beef = Ingredient.create(name: "Ground Beef", cost: 4)
+    garlic = Ingredient.create(name: "Garlic", cost: 2)
+    tomato_paste = Ingredient.create(name: "Tomato Paste", cost: 1)
+    
+    visit "/ingredients"
+    
+    expect("Tomato Paste").to appear_before("Garlic")
+    expect("Garlic").to appear_before("Ground Beef")
+  end
 end
