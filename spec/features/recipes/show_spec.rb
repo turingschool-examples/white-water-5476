2c8d7ea9dcbe8634c04fa9efc43f4057796c94af
Dylan Perry <dylanbperry@gmail.com>
2023-12-10 15:48:42 -0800

    Add total_ingredient_cost

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -1,29 +1,37 @@
 require "rails_helper"
 
 RSpec.describe "the recipe show page" do
-  it "shows the recipe and all its attributes" do
-    beef_pasta = Recipe.create(name: "Beef Pasta", complexity: 2, genre: "Italian")
+    before(:each) do
+        @beef_pasta = Recipe.create(name: "Beef Pasta", complexity: 2, genre: "Italian")
+        @ground_beef = @beef_pasta.ingredients.create(name: "Ground Beef", cost: 4)
+        @garlic = @beef_pasta.ingredients.create(name: "@garlic", cost: 2)
+        @tomato_paste = @beef_pasta.ingredients.create(name: "Tomato Paste", cost: 1)
+    end
+    
+    it "shows the recipe and all its attributes" do
+        visit "/recipes/#{@beef_pasta.id}"
 
-    visit "/recipes/#{beef_pasta.id}"
+        expect(page).to have_content(@beef_pasta.name)
+        expect(page).to have_content(@beef_pasta.complexity)
+        expect(page).to have_content(@beef_pasta.genre)
+    end
 
-    expect(page).to have_content(beef_pasta.name)
-    expect(page).to have_content(beef_pasta.complexity)
-    expect(page).to have_content(beef_pasta.genre)
-  end
+    it "shows all of a recipe's ingredients, and their attributes" do
+        visit "/recipes/#{@beef_pasta.id}"
 
-  it "shows all of a recipe's ingredients, and their attributes" do
-    beef_pasta = Recipe.create(name: "Beef Pasta", complexity: 2, genre: "Italian")
-    ground_beef = beef_pasta.ingredients.create(name: "Ground Beef", cost: 4)
-    garlic = beef_pasta.ingredients.create(name: "Garlic", cost: 2)
-    tomato_paste = beef_pasta.ingredients.create(name: "Tomato Paste", cost: 1)
+        expect(page).to have_content(@ground_beef.name)
+        # expect(page).to have_content(@ground_beef.cost)
+        expect(page).to have_content(@garlic.name)
+        # expect(page).to have_content(@garlic.cost)
+        expect(page).to have_content(@tomato_paste.name)
+        # expect(page).to have_content(@tomato_paste.cost)
+    end
 
-    visit "/recipes/#{beef_pasta.id}"
+    it "shows the total cost of all ingredients in a recipe" do
+        visit "/recipes/#{@beef_pasta.id}"
 
-    expect(page).to have_content(ground_beef.name)
-    # expect(page).to have_content(ground_beef.cost)
-    expect(page).to have_content(garlic.name)
-    # expect(page).to have_content(garlic.cost)
-    expect(page).to have_content(tomato_paste.name)
-    # expect(page).to have_content(tomato_paste.cost)
-  end
+        total_cost = (@ground_beef.cost + @garlic.cost + @tomato_paste.cost)
+        
+        expect(page).to have_content(total_cost)
+    end
 end
