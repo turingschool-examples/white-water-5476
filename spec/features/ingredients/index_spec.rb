d3bc1a7b552cf3aef86f745026b2a67cd6ebeefa
Dylan Perry <dylanbperry@gmail.com>
2023-12-10 14:37:19 -0800

    Create and save ingredient objects

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 describe "the ingredients index page" do
     it "lists all the ingredients with their attributes" do
-      ground_beef = Ingredient.new(name: "Ground Beef", cost: 4)
-      garlic = Ingredient.new(name: "Garlic", cost: 2)
-      tomato_paste = Ingredient.new(name: "Tomato Paste", cost: 1)
+      ground_beef = Ingredient.create(name: "Ground Beef", cost: 4)
+      garlic = Ingredient.create(name: "Garlic", cost: 2)
+      tomato_paste = Ingredient.create(name: "Tomato Paste", cost: 1)
       
       visit "/ingredients"
 
