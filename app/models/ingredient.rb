6af19001655c1baf7caf133b71a679e2f430f0ba
Dylan Perry <dylanbperry@gmail.com>
2023-12-10 15:23:41 -0800

    Add has_many associations between ingredients and recipes tables

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ApplicationRecord
+   has_many :recipe_ingredients
+   has_many :recipes, through: :recipe_ingredients
    validates_presence_of :name, :cost
 
    def self.order_by_recently_created
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
+   has_many :recipe_ingredients
+   has_many :ingredients, through: :recipe_ingredients
    validates_presence_of :name, :complexity, :genre
 end
