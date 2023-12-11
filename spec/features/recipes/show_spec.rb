3a6e431ea336485147b66a290a3eba2be53488e1
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-11 18:07:10 -0500

    Recieve new error while trying to correct the test

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe "the recipes show" do
   before(:each) do
     @recipe = Recipe.create!(name: "Salted Ground Beef", complexity: 1, genre: "Carnivore")
-    @ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 2)
-    @ingredient_2 = Ingredient.create!(name: "Salt", cost: 4)
+    @ingredient_1 = @recipe.ingredients.create!(name: "Ground Beef", cost: 2)
+    @ingredient_2 = @recipe.ingredients.create!(name: "Salt", cost: 4)
   end
 
   it "lists all the recipes attributes" do
@@ -18,13 +18,15 @@ RSpec.describe "the recipes show" do
   it "lists all the recipes ingredients" do
     visit "/recipes/#{@recipe.id}"
     
-    expect(page).to have_content(@recipe.ingredient.name)
-    expect(page).to have_content(@recipe.ingredient.cost)
+    expect(page).to have_content("Ground Beef")
+    expect(page).to have_content(2)
+    expect(page).to have_content("Ground Beef")
+    expect(page).to have_content(4)
   end
 
   it "shows the total cost of all ingredients in the recipe" do
     visit "/recipes/#{@recipe.id}"
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content("Total Cost: 6")
   end
 
