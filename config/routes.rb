db8759fc958f817795fe3e7eaa9429fea7e16e56
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-12-10 21:15:45 -0700

    Add ingredients controller and view with @ingredients.each

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/", to: "application#welcome"
+  # get "/", to: "ingredient#welcome"
 
-  get "/ingredients", to:
+  get "/ingredients", to: "ingredients#index"
 
 
 
