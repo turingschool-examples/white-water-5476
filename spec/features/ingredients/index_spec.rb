023b1efa6ed87f3cc20bdcbec4f9074bfda04e5f
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-11 16:44:03 -0500

    User Story 1: Made progress, but stopped to incorrectly named controller file

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "the ingredients index" do
+  before(:each) do
+    @ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 2)
+    @ingredient_2 = Ingredient.create!(name: "Salt", cost: 4)
+  end
+
+  it "lists all the ingredients attributes" do
+    visit "/ingredients"
+
+    expect(page).to have_content(@ingredient_1.name)
+    expect(page).to have_content(@ingredient_1.cost)
+    expect(page).to have_content(@ingredient_2.name)
+    expect(page).to have_content(@ingredient_2.cost)
+
+  end
+
+end
