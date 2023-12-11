eab9c0f967e3530de0424690c2bae09d76a914fb
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-11 17:33:26 -0500

    feat: Create a totalling method in the Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,12 @@ class Recipe < ApplicationRecord
    validates_presence_of :name, :complexity, :genre
    has_many :recipe_ingredients
    has_many :ingredients, through: :recipe_ingredients
+
+   def total_cost
+      total = 0
+      @ingredients.each do |ingredient|
+         total += ingredient.cost
+      end
+      total
+   end
 end
