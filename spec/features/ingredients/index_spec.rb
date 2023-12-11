85e26069cc0cc8e8f5a36f6d23a3c9e37c8ef3c2
Martin Chavez <martinchavez1203@gmail.com>
2023-12-10 17:49:35 -0700

    add test for US 1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe 'Ingredients Index' do
+  it 'visitor views a list of ingrediets with name and cost' do
+    ingrediet1 = Ingredient.create(name: 'Ground Beef', cost: 2)
+    ingrediet2 = Ingredient.create(name: 'Salt', cost: 4)
+
+    visit "/ingredients"
+
+    
+    expect(page).to have_content(ingrediet1.name)
+    expect(page).to have_content(ingrediet1.cost)
+    
+    
+    expect(page).to have_content(ingrediet2.name)
+    expect(page).to have_content(ingrediet2.cost)
+  end
+end
