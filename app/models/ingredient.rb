45d51e10fba0b85545d64bc7381be762d83d224b
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-11 18:30:43 -0500

    Complete Extension 1

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,8 @@ class Ingredient < ApplicationRecord
    validates_presence_of :name, :cost
    has_many :recipe_ingredients
    has_many :recipes, through: :recipe_ingredients
+
+   def self.sort_alphabetically
+      order(name: :asc)
+    end
 end
