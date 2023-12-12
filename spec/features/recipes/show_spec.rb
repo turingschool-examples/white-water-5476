fceb39b18c7370bb5a38d9f45f75294878529aab
Victor Sanchez <vsanantone@gmail.com>
2023-12-12 15:26:42 -0600

    US#2 Recipes Show displays recipes name, complexity and genre and a list of all the ingredients for the recipe

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-RSpec.describe "Ingredients Index" do
-  it "displays all ingredients and their cost" do
+RSpec.describe "Recipes Show" do
+  it "displays a Recipes name, complexity and genre and a list of all the ingredients for the recipe" do
     # User Story 2 - Recipes Show
-    enchiladas = Recipe.create!(name: "Enchiladas", complexity: "Medium", genre: "Tex-Mex")
+    enchiladas = Recipe.create!(name: "Enchiladas", complexity: 4, genre: "Tex-Mex")
 
     ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
     cheese = Ingredient.create!(name: "Cheese", cost: 3)
@@ -14,14 +14,15 @@ RSpec.describe "Ingredients Index" do
     # As a visitor,
     # When I visit '/recipes/:id',
     visit "/recipes/#{enchiladas.id}"
+
     # Then I see the recipe's name, complexity and genre,
     expect(page).to have_content("Recipe ##{enchiladas.id}")
     expect(page).to have_content("Name: Enchiladas")
-    expect(page).to have_content("Complexity: Medium")
+    expect(page).to have_content("Complexity: 4")
     expect(page).to have_content("Genre: Tex-Mex")
-    # and I see a list of the names of the ingredients for the recipe.
-    expect(page).to have_content("Ingredients for #{enchiladas.name}")
 
+    # and I see a list of the names of the ingredients for the recipe.
+    expect(page).to have_content("Ingredients for Enchiladas")
     expect(page).to have_content("Ground Beef")
     expect(page).to have_content("Cheese")
     expect(page).to have_content("Tomatillo")
