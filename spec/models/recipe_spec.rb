f090a415ae6f26373e79566c9da19792ba0550b9
Barry Allahyar <barrya@gmail.com>
2023-12-12 20:25:04 -0600

    Test: Add test for #toal_cost in models/recipe_spec.rb

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe 'total costs of ingedients method' do
+    it 'can sum the #total_cost of ingredients' do
+    recipe_1 = Recipe.create(name: "Ghormesabzi", complexity: 5, genre: "Persian")
+    
+    ingredient_1 = Ingredient.create(name: "Rice", cost: 1)
+    ingredient_2 = Ingredient.create(name: "Saffron", cost: 4)
+    ingredient_3 = Ingredient.create(name: "Butter", cost: 1)
+    ingredient_4 = Ingredient.create(name: "Cilantro", cost: 2)
+    ingredient_5 = Ingredient.create(name: "Parsley", cost: 2)
+
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_1)
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_2)
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_3)
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_4)
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_5)
+
+    expect(recipe_1.total_cost).to eq(10)
+    end
+  end
 end
