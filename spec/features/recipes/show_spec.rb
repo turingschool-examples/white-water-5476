bc7f1373b457ba8b2bc8dee45ed2e5f3d696a397
Barry Allahyar <barrya@gmail.com>
2023-12-12 20:21:08 -0600

    Test: Add test for #total_cost in recipes/show_spec.rb

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -42,4 +42,24 @@ describe 'Recipe Show Page' do
     expect(page).to have_content(ingredient_4.name)
     expect(page).to have_content(ingredient_5.name)
   end
+
+  it 'shows a list of the names of the ingredients for the recipe' do
+    recipe_1 = Recipe.create(name: "Ghormesabzi", complexity: 5, genre: "Persian")
+    
+    ingredient_1 = Ingredient.create(name: "Rice", cost: 1)
+    ingredient_2 = Ingredient.create(name: "Saffron", cost: 4)
+    ingredient_3 = Ingredient.create(name: "Butter", cost: 1)
+    ingredient_4 = Ingredient.create(name: "Cilantro", cost: 2)
+    ingredient_5 = Ingredient.create(name: "Parsley", cost: 2)
+
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_1)
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_2)
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_3)
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_4)
+    RecipeIngredient.create(recipe: recipe_1, ingredient: ingredient_5)
+
+    visit "recipes/#{recipe_1.id}"
+
+    expect(page).to have_content("Total Cost: 10")
+  end
 end
